1448aa12edaeafcced533b9407125ecefb5ec498
Timothy Redmond <tredmond@stanford.edu>
2009-09-27 00:22:15 +0000

    test is more complete and rules out things like confusion of source and target.

--- junit/org/protege/owl/prompt2/diff/AlgorithmTest.java
+++ junit/org/protege/owl/prompt2/diff/AlgorithmTest.java
@@ -1,18 +1,19 @@
 package org.protege.owl.prompt2.diff;
 
 import java.io.File;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.protege.owl.prompt2.diff.algorithms.MatchByCode;
 import org.protege.owl.prompt2.diff.algorithms.MatchById;
 import org.protege.owl.prompt2.diff.algorithms.MatchStandardVocabulary;
 import org.protege.owl.prompt2.diff.algorithms.SuperSubClassPinch;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -80,11 +81,17 @@ public class AlgorithmTest extends TestCase {
         JunitUtilities.printDivider();
         loadOntologies("ParentsAndChildren");
         Properties parameters = new Properties();
+        parameters.setProperty(MatchByCode.USE_CODE_PROPERTY, 
+                               "http://www.tigraworld.com/protege/ParentsAndChildren.owl#code");
         parameters.setProperty(SuperSubClassPinch.REQUIRED_SUBCLASSES_PROPERTY, "2");
         Engine e = new Engine(factory, ontology1, ontology2, parameters);
         CountEntityMatchesListener listener = new CountEntityMatchesListener();
         e.getOwlDiffMap().addDiffListener(listener);
-        e.setDiffAlgorithms(new DiffAlgorithm[] { new MatchById(), new SuperSubClassPinch() });
+        e.setDiffAlgorithms(new DiffAlgorithm[] { 
+                new MatchByCode(),
+                new MatchStandardVocabulary(),
+                new SuperSubClassPinch() 
+        });
         
         e.run();
         
@@ -93,5 +100,12 @@ public class AlgorithmTest extends TestCase {
         assertTrue(diffs.getUnmatchedTargetEntities().size() == 0);
         assertTrue(diffs.getUnmatchedSourceAxioms().isEmpty());
         assertTrue(diffs.getUnmatchedTargetAxioms().isEmpty());
+        
+        List<Set<OWLEntity>> matches = listener.getEntityMatches();
+        assertTrue(matches.size() == 4);
+        assertTrue(matches.get(2).size() == 1);
+        assertTrue(matches.get(2).iterator().next().getIRI().getFragment().equals("BL"));
+        assertTrue(matches.get(3).size() == 1);
+        assertTrue(matches.get(3).iterator().next().getIRI().getFragment().equals("EL"));
     }
 }
