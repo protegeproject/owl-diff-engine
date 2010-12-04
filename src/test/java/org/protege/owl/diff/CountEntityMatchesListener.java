054aeec0718dde0bd80bbaba9cf063f76e93e04d
Timothy Redmond <tredmond@stanford.edu>
2010-12-04 00:33:44 +0000

    working on the standard maven layout

---
+++ src/test/java/org/protege/owl/diff/CountEntityMatchesListener.java
@@ -0,0 +1,52 @@
+package org.protege.owl.diff;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.protege.owl.diff.raw.DiffListener;
+import org.protege.owl.diff.raw.UnmatchedAxiom;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+
+public class CountEntityMatchesListener implements DiffListener {
+    private List<Set<OWLEntity>> entityMatches = new ArrayList<Set<OWLEntity>>();
+    
+    public List<Set<OWLEntity>> getEntityMatches() {
+        return Collections.unmodifiableList(entityMatches);
+    }
+
+    public void addMatch(OWLEntity source, OWLEntity target) {
+        entityMatches.add(Collections.singleton(source));
+    }
+
+    public void addMatchingEntities(Map<OWLEntity, OWLEntity> newMatches) {
+        entityMatches.add(new HashSet<OWLEntity>(newMatches.keySet()));
+    }
+
+    public void addMatch(OWLAnonymousIndividual source, OWLAnonymousIndividual target) {
+
+    }
+
+    public void addMatchingAnonymousIndividuals(Map<OWLAnonymousIndividual, OWLAnonymousIndividual> newMatches) {
+
+    }
+    
+    public void unmatchedAxiomMoved(UnmatchedAxiom unmatched) {
+
+    }
+
+    public void addMatchedAxiom(OWLAxiom axiom) {
+
+    }
+
+    public void addUnmatcheableAxiom(OWLAxiom axiom) {
+
+    }
+
+}
