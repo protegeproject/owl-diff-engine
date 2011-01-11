86c0d3a751b78723c1c7573b4d440c12d72d0dba
Timothy Redmond <tredmond@stanford.edu>
2011-01-11 01:54:04 +0000

    added cases for the owl deprecated property and for a list of properties that are added to retired classes to provide meta information.

--- src/main/java/org/protege/owl/diff/service/RetirementClassService.java
+++ src/main/java/org/protege/owl/diff/service/RetirementClassService.java
@@ -1,6 +1,8 @@
 package org.protege.owl.diff.service;
 
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import org.protege.owl.diff.Engine;
 import org.protege.owl.diff.align.OwlDiffMap;
@@ -8,18 +10,23 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 
-public class RetirementClassService {
+public final class RetirementClassService {
 
     public static final String RETIREMENT_CLASS_PROPERTY  = "retirement.class.prefix";
     public static final String RETIREMENT_STATUS_PROPERTY = "retirement.status.property";
     public static final String RETIREMENT_STATUS_STRING   = "retirement.status.string";
+    public static final String RETIREMENT_META_PROPERTIES = "retirement.meta.property.";
     
     private String retirementClassPrefix;
     private OWLAnnotationProperty retirementStatusProperty;
+    private Set<OWLAnnotationProperty> retirementMetaProperties;
     private String retirementStatusString;
+    
+    private OWLDataFactory factory;
     
     public static RetirementClassService getRetirementClassService(Engine e) {
     	RetirementClassService rcs = e.getService(RetirementClassService.class);
@@ -31,11 +38,21 @@ public class RetirementClassService {
     }
     
     private RetirementClassService(OwlDiffMap diffMap, Properties parameters) {
+    	factory = diffMap.getOWLDataFactory();
     	retirementClassPrefix = (String) parameters.get(RETIREMENT_CLASS_PROPERTY);
     	String retirementStatusPropertyName = (String) parameters.get(RETIREMENT_STATUS_PROPERTY);
     	retirementStatusString = (String) parameters.getProperty(RETIREMENT_STATUS_STRING);
     	if (retirementStatusPropertyName != null && retirementStatusString != null) {
     		retirementStatusProperty = diffMap.getOWLDataFactory().getOWLAnnotationProperty(IRI.create(retirementStatusPropertyName));
+    	}
+    	retirementMetaProperties = new HashSet<OWLAnnotationProperty>();
+    	int i = 0;
+    	while (true) {
+    		String retirementMetaProperty = (String) parameters.getProperty(RETIREMENT_META_PROPERTIES + (i++));
+    		if (retirementMetaProperty == null) {
+    			break;
+    		}
+    		retirementMetaProperties.add(factory.getOWLAnnotationProperty(IRI.create(retirementMetaProperty)));
     	}
     }
     
@@ -53,9 +70,18 @@ public class RetirementClassService {
     	}
     	else if (axiom instanceof OWLAnnotationAssertionAxiom && retirementStatusProperty != null) {
     		OWLAnnotationAssertionAxiom annotationAxiom = (OWLAnnotationAssertionAxiom) axiom;
-    		ret = annotationAxiom.getAnnotation().getProperty().equals(retirementStatusProperty) 
-    				&& annotationAxiom.getAnnotation().getValue() instanceof OWLLiteral
-    				&& ((OWLLiteral) annotationAxiom.getAnnotation().getValue()).getLiteral().equals(retirementStatusString);
+    		OWLAnnotationProperty property = annotationAxiom.getAnnotation().getProperty();
+    		if (property.equals(retirementStatusProperty)) {
+    			ret = annotationAxiom.getAnnotation().getValue() instanceof OWLLiteral
+    		             && ((OWLLiteral) annotationAxiom.getAnnotation().getValue()).getLiteral().equals(retirementStatusString);
+    		}
+    		else if (property.equals(factory.getOWLDeprecated())) {
+    			ret = annotationAxiom.getAnnotation().getValue() instanceof OWLLiteral
+	             		&& ((OWLLiteral) annotationAxiom.getAnnotation().getValue()).getLiteral().toLowerCase().equals("true");
+    		}
+    		else if (retirementMetaProperties.contains(property)) {
+    			ret = true;
+    		}
     	}
     	return ret;
     }
