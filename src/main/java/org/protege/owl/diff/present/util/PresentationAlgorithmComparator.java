136245a57ab8575d67e771f69aa5f2a557e500c9
Timothy Redmond <tredmond@stanford.edu>
2011-02-18 05:37:24 +0000

    minor refactor

--- src/main/java/org/protege/owl/diff/present/util/PresentationAlgorithmComparator.java
+++ src/main/java/org/protege/owl/diff/present/util/PresentationAlgorithmComparator.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 import org.protege.owl.diff.present.PresentationAlgorithm;
 
 public class PresentationAlgorithmComparator implements Comparator<PresentationAlgorithm> {
-    public static final int MIN_PRIORITY = 1;
-    public static final int DEFAULT_PRIORITY = 5;
-    public static final int MAX_PRIORITY = 10;
+    public static final int MIN_ALGORITHM_PRIORITY = 1;
+    public static final int DEFAULT_ALGORITHM_PRIORITY = 5;
+    public static final int MAX_ALGORITHM_PRIORITY = 10;
 
     /*
      * ordering is the inverse of the obvious so that high priority algorithms come first.
@@ -28,11 +28,11 @@ public class PresentationAlgorithmComparator implements Comparator<PresentationA
     
     private int getPriority(PresentationAlgorithm aa) {
         int priority = aa.getPriority();
-        if (priority < MIN_PRIORITY) {
-            priority = MIN_PRIORITY;
+        if (priority < MIN_ALGORITHM_PRIORITY) {
+            priority = MIN_ALGORITHM_PRIORITY;
         }
-        if (priority > MAX_PRIORITY) {
-            priority = MAX_PRIORITY;
+        if (priority > MAX_ALGORITHM_PRIORITY) {
+            priority = MAX_ALGORITHM_PRIORITY;
         }
         return priority;
     }
