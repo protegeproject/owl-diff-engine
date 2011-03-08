8e2ad4d577f7a6cf811085fbf973507d5cc05cee
Timothy Redmond <tredmond@stanford.edu>
2011-03-08 01:22:50 +0000

    common super/sub class methods needed by a few sibling algorithms.

---
+++ src/main/java/org/protege/owl/diff/service/SiblingService.java
@@ -0,0 +1,62 @@
+package org.protege.owl.diff.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.protege.owl.diff.Engine;
+import org.protege.owl.diff.align.OwlDiffMap;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * This class provides a trivial service - it gets subclasses and superclasses.  It is here as a marker
+ * for the time when we might add some inference to this class.  In particular, one might expect to see the 
+ * CheeseyPizza inference and an inference that classes without a parent are subclasses of OWL thing.  Adding
+ * these capabilities will make this services more expensive so getting this service needs to be deferred until
+ * it is known that it is needed.
+ */
+
+public class SiblingService {
+	private OwlDiffMap diffs;
+	
+	
+	public static SiblingService get(Engine e) {
+		SiblingService s = e.getService(SiblingService.class);
+		if (s == null) {
+			s = new SiblingService(e);
+			e.addService(s);
+		}
+		return s;
+	}
+	
+	private SiblingService(Engine e) {
+		diffs = e.getOwlDiffMap();
+	}
+	
+	
+	public Set<OWLClass> getSourceSuperClasses(OWLClass c) {
+		Set<OWLClass> superClasses = new HashSet<OWLClass>();
+		for (OWLClassExpression ce : c.getSuperClasses(diffs.getSourceOntology())) {
+			if (!ce.isAnonymous()) {
+				superClasses.add(ce.asOWLClass());
+			}
+		}
+		return superClasses;
+	}
+	
+	
+	public Set<OWLClass> getSubClasses(OWLClass c, boolean isSourceOntology) {
+		Set<OWLClass> subClasses = new HashSet<OWLClass>();
+		for (OWLClassExpression ce : c.getSubClasses(getOntology(isSourceOntology))) {
+			if (!ce.isAnonymous()) {
+				subClasses.add(ce.asOWLClass());
+			}
+		}
+		return subClasses;
+	}
+	
+	private OWLOntology getOntology(boolean isSourceOntology) {
+		return isSourceOntology ? diffs.getSourceOntology() : diffs.getTargetOntology();
+	}
+}
