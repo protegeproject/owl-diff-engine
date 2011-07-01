b167bd5aed8a7363f95241fb3aa7423d40a404eb
Timothy Redmond <tredmond@stanford.edu>
2011-07-01 03:05:52 +0000

    removed the getPriority

--- src/main/java/org/protege/owl/diff/align/AlignmentAlgorithm.java
+++ src/main/java/org/protege/owl/diff/align/AlignmentAlgorithm.java
@@ -3,18 +3,13 @@ package org.protege.owl.diff.align;
 import org.protege.owl.diff.Engine;
 
 
-public interface AlignmentAlgorithm {
+public interface AlignmentAlgorithm extends Prioritized {
     
     void initialise(Engine e);
     
     void run();
     
     void reset();
-    
-    /** 
-     * return the priority of the algorithm as an integer from 0 to 10.
-     */
-    int getPriority();
     
     AlignmentAggressiveness getAggressiveness();
     
