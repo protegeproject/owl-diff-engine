c10ead6a9de2294110b56288941ded47d435da9c
Timothy Redmond <tredmond@stanford.edu>
2011-07-01 03:03:12 +0000

    struggling to figure out how the rendering service should work

--- src/main/java/org/protege/owl/diff/service/RenderingService.java
+++ src/main/java/org/protege/owl/diff/service/RenderingService.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 
+import org.protege.owl.diff.DifferencePosition;
 import org.protege.owl.diff.Engine;
+import org.protege.owl.diff.align.OwlDiffMap;
 import org.protege.owl.diff.present.EntityBasedDiff;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.util.IRIShortFormProvider;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 
@@ -16,6 +20,8 @@ import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxObjec
 
 public class RenderingService {
 	private OWLDataFactory factory;
+	private Engine e;
+	private DeprecationDeferralService dds;
 	
 	private WriterDelegate sourceWriter = new WriterDelegate();
 	private ManchesterOWLSyntaxObjectRenderer sourceRenderer;
@@ -26,14 +32,16 @@ public class RenderingService {
 	public static RenderingService get(Engine e) {
 		RenderingService renderer = e.getService(RenderingService.class);
 		if (renderer == null) {
-			renderer = new RenderingService(e.getOWLDataFactory());
+			renderer = new RenderingService(e);
 			e.addService(renderer);
 		}
 		return renderer;
 	}
 	
-	private RenderingService(OWLDataFactory factory) {
-		this.factory = factory;
+	private RenderingService(Engine e) {
+		this.e = e;
+		this.factory = e.getOWLDataFactory();
+		this.dds = DeprecationDeferralService.get(e);
 	}
 	
 	public void setSourceShortFormProvider(ShortFormProvider sourceShortFormProvider) {
@@ -52,21 +60,24 @@ public class RenderingService {
 		targetRenderer = getRenderer(targetWriter, targetShortFormProvider, targetIRIShortFormProvider);
 	}
 	
+	public boolean isReady() {
+		return sourceRenderer != null && targetRenderer != null;
+	}
+	
 	
 	public String renderSourceObject(OWLObject o) {
-		return render(o, false);
+		return render(o, DifferencePosition.SOURCE);
 	}
 	
 	public String renderTargetObject(OWLObject o) {
-		return render(o, true);
+		return render(o, DifferencePosition.TARGET);
 	}
 	
 	public String renderDiff(EntityBasedDiff diff) {
 		StringBuffer diffDescription = new StringBuffer();
 		switch (diff.getDiffType()) {
 		case CREATED:
-			diffDescription.append("Created ");
-			diffDescription.append(renderTargetObject(diff.getTargetEntity()));
+			renderCreated(diffDescription, diff.getTargetEntity());
 			break;
 		case DELETED:
 			diffDescription.append("Deleted ");
@@ -82,11 +93,23 @@ public class RenderingService {
 		}
 		return diffDescription.toString();
 	}
+	
+	private void renderCreated(StringBuffer diffDescription, OWLEntity created) {
+		OWLOntology sourceOntology = e.getOwlDiffMap().getSourceOntology();
+		if (sourceOntology.containsEntityInSignature(created) && dds.checkDeprecation(created, created)) {
+			diffDescription.append("Deprecated and Replaced ");
+			diffDescription.append(renderTargetObject(created));
+		}
+		else {
+			diffDescription.append("Created ");
+			diffDescription.append(renderTargetObject(created));
+		}
+	}
 
-	private String render(OWLObject o, boolean isTargetOntology) {
-		WriterDelegate writer = isTargetOntology ? targetWriter : sourceWriter;
+	private String render(OWLObject o, DifferencePosition position) {
+		WriterDelegate writer = (position == DifferencePosition.TARGET) ? targetWriter : sourceWriter;
 		writer.reset();
-		ManchesterOWLSyntaxObjectRenderer renderer = isTargetOntology ? targetRenderer : sourceRenderer;
+		ManchesterOWLSyntaxObjectRenderer renderer = (position == DifferencePosition.TARGET) ? targetRenderer : sourceRenderer;
 		o.accept(renderer);
 		return writer.toString();
 	}
