e2f0c0ba1e6498bea2b6a98548212ddaca0b7a5b
Timothy Redmond <tredmond@stanford.edu>
2011-07-01 03:04:44 +0000

    minor refactor

---
+++ src/main/java/org/protege/owl/diff/align/Prioritized.java
@@ -0,0 +1,9 @@
+package org.protege.owl.diff.align;
+
+public interface Prioritized {
+
+    /** 
+     * return the priority of the algorithm as an integer from 0 to 10.
+     */
+	int getPriority();
+}
