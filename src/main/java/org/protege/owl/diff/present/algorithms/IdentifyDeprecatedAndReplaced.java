e9151bf4ca1d4c02b2ea8e16fa46b2a737bedbed
Timothy Redmond <tredmond@stanford.edu>
2011-07-12 23:46:16 +0000

    minor refactors to ensure that this cooperates well with the identify deprecated routine

--- src/main/java/org/protege/owl/diff/present/algorithms/IdentifyDeprecatedAndReplaced.java
+++ src/main/java/org/protege/owl/diff/present/algorithms/IdentifyDeprecatedAndReplaced.java
@@ -6,7 +6,6 @@ import org.protege.owl.diff.present.Changes;
 import org.protege.owl.diff.present.EntityBasedDiff;
 import org.protege.owl.diff.present.EntityBasedDiff.DiffType;
 import org.protege.owl.diff.present.util.PresentationAlgorithmComparator;
-import org.protege.owl.diff.present.MatchDescription;
 import org.protege.owl.diff.present.MatchedAxiom;
 import org.protege.owl.diff.service.DeprecationDeferralService;
 import org.semanticweb.owlapi.model.OWLAxiom;
@@ -14,15 +13,13 @@ import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLEntity;
 
 public class IdentifyDeprecatedAndReplaced extends AbstractAnalyzerAlgorithm {
-	public static final MatchDescription AXIOM_IS_DEPRECATION = new MatchDescription("Deprecated", MatchDescription.PRIMARY_MATCH_PRIORITY);
-	
 	private OWLDataFactory factory;
 	private OwlDiffMap diffMap;
 	private Changes changes;
 	private DeprecationDeferralService dds;
 	
 	public IdentifyDeprecatedAndReplaced() {
-		setPriority(PresentationAlgorithmComparator.MAX_ALGORITHM_PRIORITY);
+		setPriority(IdentifyDeprecatedEntity.IDENTIFY_DEPRECATED_ENTITY_PRIORITY + 1);
 	}
 	
 	public void initialise(Engine e) {
@@ -45,7 +42,7 @@ public class IdentifyDeprecatedAndReplaced extends AbstractAnalyzerAlgorithm {
 				&& dds.checkDeprecation(created, created)) {
 			diff.setDiffTypeDescription("Deprecated and replaced");
 			OWLAxiom declaration = factory.getOWLDeclarationAxiom(created);
-			OWLAxiom deprecated  = factory.getOWLAnnotationAssertionAxiom(created.getIRI(), factory.getOWLAnnotation(factory.getOWLDeprecated(), factory.getOWLLiteral(true)));
+			OWLAxiom deprecated  = factory.getOWLAnnotationAssertionAxiom(created.getIRI(), IdentifyDeprecatedEntity.DEPRECATE_ANNOTATION);
 			MatchedAxiom declarationMatch = null;
 			MatchedAxiom deprecationMatch = null;
 			for (MatchedAxiom match : diff.getAxiomMatches()) {
@@ -60,7 +57,7 @@ public class IdentifyDeprecatedAndReplaced extends AbstractAnalyzerAlgorithm {
 			}
 			if (deprecationMatch != null) {
 				changes.removeMatch(deprecationMatch);
-				changes.addMatch(new MatchedAxiom(null, deprecationMatch.getTargetAxiom(), AXIOM_IS_DEPRECATION));
+				changes.addMatch(new MatchedAxiom(null, deprecationMatch.getTargetAxiom(), IdentifyDeprecatedEntity.AXIOM_IS_DEPRECATION));
 			}
 			if (declarationMatch != null) {
 				changes.removeMatch(declarationMatch);
