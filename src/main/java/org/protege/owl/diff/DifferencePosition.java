e756f996102f1c51bc1d99362fcceaa38e866d0a
Timothy Redmond <tredmond@stanford.edu>
2011-07-12 23:42:28 +0000

    added more utilities to this difference position (source vs. target enum) thingy

--- src/main/java/org/protege/owl/diff/DifferencePosition.java
+++ src/main/java/org/protege/owl/diff/DifferencePosition.java
@@ -3,6 +3,9 @@ package org.protege.owl.diff;
 import java.util.Set;
 
 import org.protege.owl.diff.align.OwlDiffMap;
+import org.protege.owl.diff.present.EntityBasedDiff;
+import org.protege.owl.diff.present.MatchedAxiom;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLOntology;
 
@@ -15,6 +18,14 @@ public enum DifferencePosition {
 		public Set<OWLEntity> getUnmatchedEntities(OwlDiffMap diffMap) {
 			return diffMap.getUnmatchedSourceEntities();
 		}
+		
+		public OWLEntity getEntity(EntityBasedDiff diff) {
+			return diff.getSourceEntity();
+		}
+		
+		public OWLAxiom getAxiom(MatchedAxiom match) {
+			return match.getSourceAxiom();
+		}
 	},
 	TARGET {
 		public OWLOntology getOntology(OwlDiffMap diffMap) {
@@ -24,9 +35,20 @@ public enum DifferencePosition {
 		public Set<OWLEntity> getUnmatchedEntities(OwlDiffMap diffMap) {
 			return diffMap.getUnmatchedTargetEntities();
 		}
+		
+		public OWLEntity getEntity(EntityBasedDiff diff) {
+			return diff.getTargetEntity();
+		}
+		
+		public OWLAxiom getAxiom(MatchedAxiom match) {
+			return match.getTargetAxiom();
+		}
 	};
 	
 	public abstract OWLOntology getOntology(OwlDiffMap diffMap);
 	public abstract Set<OWLEntity> getUnmatchedEntities(OwlDiffMap diffMap);
 	
+	public abstract OWLEntity getEntity(EntityBasedDiff diff);
+	
+	public abstract OWLAxiom  getAxiom(MatchedAxiom match);
 }
