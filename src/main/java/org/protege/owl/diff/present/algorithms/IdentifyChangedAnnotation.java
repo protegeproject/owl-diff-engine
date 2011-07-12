ddff98c840577900cb4c35f835cc2958970175c2
Timothy Redmond <tredmond@stanford.edu>
2011-07-12 23:47:36 +0000

    Annotation assertion axioms are now a pain (now that the subject of an annotation assertion axiom is not an entity).  This routine has been tightened up and the identify refactored annotations will take over when it is appropriate.

--- src/main/java/org/protege/owl/diff/present/algorithms/IdentifyChangedAnnotation.java
+++ src/main/java/org/protege/owl/diff/present/algorithms/IdentifyChangedAnnotation.java
@@ -5,19 +5,28 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.protege.owl.diff.DifferencePosition;
 import org.protege.owl.diff.Engine;
 import org.protege.owl.diff.present.Changes;
 import org.protege.owl.diff.present.EntityBasedDiff;
 import org.protege.owl.diff.present.MatchDescription;
 import org.protege.owl.diff.present.MatchedAxiom;
+import org.protege.owl.diff.present.util.PresentationAlgorithmComparator;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLEntity;
 
 public class IdentifyChangedAnnotation extends AbstractAnalyzerAlgorithm {
+	public static final int IDENTIFY_CHANGED_ANNOTATION_PRIORITY = 	PresentationAlgorithmComparator.DEFAULT_ALGORITHM_PRIORITY;
 	public static final MatchDescription CHANGED_ANNOTATION = new MatchDescription("Annotation Changed");
 	
 	private Changes changes;
 
+	public IdentifyChangedAnnotation() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public void initialise(Engine e) {
 		changes = e.getChanges();
 	}
@@ -29,21 +38,19 @@ public class IdentifyChangedAnnotation extends AbstractAnalyzerAlgorithm {
 	}
 	
 	private void match(EntityBasedDiff diff) {
-		matchAnonymousSuperClasses(diff);
+		matchAnnotations(diff);
 	}
 	
-	private void matchAnonymousSuperClasses(EntityBasedDiff diff) {
+	private void matchAnnotations(EntityBasedDiff diff) {
 		Map<OWLAnnotationProperty, Set<MatchedAxiom>> sourceAxioms = new HashMap<OWLAnnotationProperty, Set<MatchedAxiom>>();
 		Map<OWLAnnotationProperty, Set<MatchedAxiom>> targetAxioms = new HashMap<OWLAnnotationProperty, Set<MatchedAxiom>>();
 		
 		for (MatchedAxiom correspondence : diff.getAxiomMatches()) {
-			if (correspondence.getDescription().equals(MatchedAxiom.AXIOM_ADDED) &&
-					correspondence.getTargetAxiom() instanceof OWLAnnotationAssertionAxiom) {
-				addAnnotationAssertionMatch(targetAxioms, correspondence, (OWLAnnotationAssertionAxiom) correspondence.getTargetAxiom());
-			}
-			if (correspondence.getDescription().equals(MatchedAxiom.AXIOM_DELETED) &&
-					correspondence.getSourceAxiom() instanceof OWLAnnotationAssertionAxiom) {
+			if (isCandidateMatch(diff, correspondence, DifferencePosition.SOURCE)) {
 				addAnnotationAssertionMatch(sourceAxioms, correspondence, (OWLAnnotationAssertionAxiom) correspondence.getSourceAxiom());
+			}
+			if (isCandidateMatch(diff, correspondence, DifferencePosition.TARGET)) {
+				addAnnotationAssertionMatch(targetAxioms, correspondence, (OWLAnnotationAssertionAxiom) correspondence.getTargetAxiom());
 			}
 		}
 		for (OWLAnnotationProperty p : sourceAxioms.keySet()) {
@@ -53,12 +60,29 @@ public class IdentifyChangedAnnotation extends AbstractAnalyzerAlgorithm {
 					&& targetMatches != null && targetMatches.size() == 1) {
 				MatchedAxiom deleted = sourceMatches.iterator().next();
 				MatchedAxiom added   = targetMatches.iterator().next();
-				MatchedAxiom changed = new MatchedAxiom(deleted.getSourceAxiom(), added.getTargetAxiom(), CHANGED_ANNOTATION);
-				changes.addMatch(changed);
 				changes.removeMatch(deleted);
 				changes.removeMatch(added);
+				
+				MatchedAxiom changed = new MatchedAxiom(deleted.getSourceAxiom(), added.getTargetAxiom(), CHANGED_ANNOTATION);
+				changes.addMatch(changed);
 			}
 		}
+	}
+	
+	private boolean isCandidateMatch(EntityBasedDiff diff, MatchedAxiom match, DifferencePosition position) {
+		MatchDescription expectedMatchDescription = (position == DifferencePosition.SOURCE 
+				? MatchedAxiom.AXIOM_DELETED : MatchedAxiom.AXIOM_ADDED);
+		OWLEntity entity = position.getEntity(diff);
+		OWLAxiom axiom = position.getAxiom(match);
+		return match.getDescription().equals(expectedMatchDescription)
+					&& axiom instanceof OWLAnnotationAssertionAxiom
+					&& entity != null
+					&& ((OWLAnnotationAssertionAxiom) axiom).getSubject().equals(entity.getIRI());	
+	}
+	
+	private boolean isIdenticalAnnotation(MatchedAxiom deleted, MatchedAxiom added) {
+		return ((OWLAnnotationAssertionAxiom) deleted.getSourceAxiom()).getAnnotation()
+					.equals(((OWLAnnotationAssertionAxiom) added.getTargetAxiom()).getAnnotation());
 	}
 
 	
