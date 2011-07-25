d78f9ba07a3c0dd29a1e08164e7fd8e7b87062a8
Timothy Redmond <tredmond@stanford.edu>
2011-07-25 04:18:12 +0000

    trim is still needed but we need to give this some thought later

--- src/main/java/org/protege/owl/diff/align/UnmatchedSourceAxiom.java
+++ src/main/java/org/protege/owl/diff/align/UnmatchedSourceAxiom.java
@@ -12,4 +12,6 @@ public interface UnmatchedSourceAxiom {
 	public Collection<OWLAnonymousIndividual> getReferencedUnmatchedAnonymousIndividuals();
 	
 	public OWLAxiom getAxiom();
+	
+	public void trim(OwlDiffMap diffMap);
 }
