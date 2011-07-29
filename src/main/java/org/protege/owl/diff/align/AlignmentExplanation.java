15fd2a341f97a303508da58a6f2ca9b32d7e2b2c
Timothy Redmond <tredmond@stanford.edu>
2011-07-29 04:45:57 +0000

    added hasDetailedExplanation so that the detailed does not need to be calculated until it is needed

--- src/main/java/org/protege/owl/diff/align/AlignmentExplanation.java
+++ src/main/java/org/protege/owl/diff/align/AlignmentExplanation.java
@@ -3,7 +3,10 @@ package org.protege.owl.diff.align;
 import org.semanticweb.owlapi.model.OWLObject;
 
 public interface AlignmentExplanation {
+	
 	String getExplanation();
+	
+	boolean hasDetailedExplanation(OWLObject sourceObject);
 	
 	String getDetailedExplanation(OWLObject sourceObject);
 }
