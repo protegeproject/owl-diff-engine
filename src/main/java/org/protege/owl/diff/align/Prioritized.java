3ace90893b433a379bf48a35835e87d8bf03ece0
Matthew Horridge <matthew.horridge@stanford.edu>
2015-03-05 20:50:46 -0800

    Added missing return tag in javadoc

--- src/main/java/org/protege/owl/diff/align/Prioritized.java
+++ src/main/java/org/protege/owl/diff/align/Prioritized.java
@@ -3,7 +3,7 @@ package org.protege.owl.diff.align;
 public interface Prioritized {
 
     /** 
-     * return the priority of the algorithm as an integer from 0 to 10.
+     * @return the priority of the algorithm as an integer from 0 to 10.
      */
 	int getPriority();
 }
