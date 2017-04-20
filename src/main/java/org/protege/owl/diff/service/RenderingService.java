fcd78519b2a8a14aa61f996c5ae9e5880427e545
reischlb <reischl.barnabas@gmail.com>
2017-04-20 16:44:31 +0200

    Removed WriterDelegate inner class

--- src/main/java/org/protege/owl/diff/service/RenderingService.java
+++ src/main/java/org/protege/owl/diff/service/RenderingService.java
@@ -30,24 +30,24 @@ import org.semanticweb.owlapi.util.ShortFormProvider;
 
 import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxObjectRenderer;
 
-public class RenderingService {
-    public static final String NO_LANGUAGE_SET = "";
-    
+public class  RenderingService {
+	public static final String NO_LANGUAGE_SET = "";
+
 	private OWLDataFactory factory;
-	
+
 	private Engine engine;
 
 
 	//ManchesterOWLSyntaxObjectRenderer is deprecated, but i didn't find any other class to replace with it
-    //I also didn't found the source of it or a good documentation to see what it does and write an other class
-    //so i think it works now and we should not make big changes on it.
-	
-	private WriterDelegate sourceWriter = new WriterDelegate();
+	//I also didn't found the source of it or a good documentation to see what it does and write an other class
+	//so i think it works now and we should not make big changes on it.
+
+	private StringWriter sourceWriter = new StringWriter();
 	private ManchesterOWLSyntaxObjectRenderer sourceRenderer;
-	
-	private WriterDelegate targetWriter = new WriterDelegate();
+
+	private StringWriter targetWriter = new StringWriter();
 	private ManchesterOWLSyntaxObjectRenderer targetRenderer;
-	
+
 	private Map<String, OWLEntity> targetNameToEntityMap;
 
 
@@ -60,7 +60,7 @@ public class RenderingService {
 		}
 		return renderer;
 	}
-	
+
 	/*
 	 * Pardon me - I am stealing this code from Protege 4.  Dependencies make it unclear how to share it.
 	 */
@@ -69,7 +69,7 @@ public class RenderingService {
 		List<String> langs = getDefaultLanguages();
 		return getShortFormProvider(ontology, annotationProperties, langs);
 	}
-	
+
 	public static ShortFormProvider getShortFormProvider(OWLOntology ontology, List<OWLAnnotationProperty> annotationProperties, List<String> langs) {
 		Map<OWLAnnotationProperty, List<String>> preferredLanguageMap = new HashMap<>();
 		for (OWLAnnotationProperty annotationProperty : annotationProperties) {
@@ -110,7 +110,7 @@ public class RenderingService {
 		engine = e;
 		this.factory = e.getOWLDataFactory();
 	}
-	
+
 	public void setSourceShortFormProvider(ShortFormProvider sourceShortFormProvider) {
 		setSourceShortFormProvider(sourceShortFormProvider, getIRIShortFormProvider(sourceShortFormProvider));
 	}
@@ -122,16 +122,16 @@ public class RenderingService {
 	public void setTargetShortFormProvider(ShortFormProvider targetShortFormProvider) {
 		setTargetShortFormProvider(targetShortFormProvider, getIRIShortFormProvider(targetShortFormProvider));
 	}
-	
+
 	public void setTargetShortFormProvider(ShortFormProvider targetShortFormProvider, IRIShortFormProvider targetIRIShortFormProvider) {
 		targetRenderer = getRenderer(targetWriter, targetShortFormProvider, targetIRIShortFormProvider);
 	}
-	
+
 	public boolean isReady() {
 		return sourceRenderer != null && targetRenderer != null;
 	}
-	
-	
+
+
 	public String renderSourceObject(OWLObject o) {
 		if (engine.getOwlDiffMap() == null) {
 			return "";
@@ -148,39 +148,39 @@ public class RenderingService {
 			return "";
 		}
 		if (targetRenderer == null) {
-			setTargetShortFormProvider(getDefaultShortFormProvider(engine.getOwlDiffMap().getTargetOntology()));			
+			setTargetShortFormProvider(getDefaultShortFormProvider(engine.getOwlDiffMap().getTargetOntology()));
 		}
 		return render(o, DifferencePosition.TARGET);
 	}
-	
+
 	public String renderDiff(EntityBasedDiff diff) {
 		StringBuilder diffDescription = new StringBuilder();
 		diffDescription.append(diff.getDiffTypeDescription());
 		diffDescription.append(": ");
 		switch (diff.getDiffType()) {
-		case CREATED:
-			diffDescription.append(renderTargetObject(diff.getTargetEntity()));
-			break;
-		case EQUIVALENT:
-			break;
-		default:
-			diffDescription.append(renderSourceObject(diff.getSourceEntity()));
-			break;
+			case CREATED:
+				diffDescription.append(renderTargetObject(diff.getTargetEntity()));
+				break;
+			case EQUIVALENT:
+				break;
+			default:
+				diffDescription.append(renderSourceObject(diff.getSourceEntity()));
+				break;
 		}
 		return diffDescription.toString();
 	}
 
 	private String render(OWLObject o, DifferencePosition position) {
-		WriterDelegate writer = (position == DifferencePosition.TARGET) ? targetWriter : sourceWriter;
-		writer.reset();
+		StringWriter writer = (position == DifferencePosition.TARGET) ? targetWriter : sourceWriter;
+		resetWriter(writer);
 		ManchesterOWLSyntaxObjectRenderer renderer = (position == DifferencePosition.TARGET) ? targetRenderer : sourceRenderer;
 		o.accept(renderer);
 		return writer.toString();
 	}
 
-	private ManchesterOWLSyntaxObjectRenderer getRenderer(WriterDelegate writer, 
-													      final ShortFormProvider shortFormProvider, 
-													      final IRIShortFormProvider iriShortFormProvider) {
+	private ManchesterOWLSyntaxObjectRenderer getRenderer(StringWriter writer,
+														  final ShortFormProvider shortFormProvider,
+														  final IRIShortFormProvider iriShortFormProvider) {
 		return new ManchesterOWLSyntaxObjectRenderer(writer, shortFormProvider) {
 			@Override
 			public void visit(IRI iri) {
@@ -192,7 +192,7 @@ public class RenderingService {
 	// creates a new IRIShortFormProvider to the given ShortFormProvider
 	private IRIShortFormProvider getIRIShortFormProvider(final ShortFormProvider shortFormProvider) {
 		return new IRIShortFormProvider() {
-			
+
 			@Override
 			public String getShortForm(IRI uri) {
 				return shortFormProvider.getShortForm(factory.getOWLClass(uri));
@@ -224,39 +224,8 @@ public class RenderingService {
 		return targetNameToEntityMap.get(rendering);
 	}
 
-	//this is a class which contains a StringWriter, and cals it's methods
-    //Is it really neccessary to have this class insted of using a simple StringWriter?
-	private static class WriterDelegate extends Writer {
-
-        private StringWriter delegate;
-        
-		private void reset() {
-            delegate = new StringWriter();
-        }
-
-
-        @Override
-		public String toString() {
-            return delegate.getBuffer().toString();
-        }
-
-
-        @Override
-		public void close() throws IOException {
-            delegate.close();
-        }
-
-
-        @Override
-		public void flush() throws IOException {
-            delegate.flush();
-        }
-
-
-        @Override
-		public void write(char cbuf[], int off, int len) throws IOException {
-            delegate.write(cbuf, off, len);
-        }
-    }
+	private void resetWriter(StringWriter w){
+		w = new StringWriter();
+	}
 
 }
